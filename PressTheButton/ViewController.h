6e3305e8ab4a82779ac7c82548fe519c0d4e1041
JaCarson <JohnAldenCarson@gmail.com>
2016-08-24 15:10:13 -0400

    Initial Commit

---
+++ PressTheButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PressTheButton
+//
+//  Created by John Carson on 8/24/16.
+//  Copyright © 2016 John Carson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PressTheButton/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PressTheButton
+//
+//  Created by John Carson on 8/24/16.
+//  Copyright © 2016 John Carson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
